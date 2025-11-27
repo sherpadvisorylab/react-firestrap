e70fc9a40a149a724c8df424e94667f61282891c
Alessandro Stucchi <wolfgan@gmail.com>
2025-11-27 09:56:45 +0100

    Refactor AI export in integrations index for improved clarity
    - Changed the export of the AI module from default to named export for better consistency and clarity in module usage.
    - Ensured that the integration structure remains organized and easy to understand.

--- src/integrations/index.ts
+++ src/integrations/index.ts
@@ -1,5 +1,5 @@
 export {default as db } from './google/firedatabase';
 export {default as storage } from './google/firestorage';
 export {dropBox, useDropBoxConnect, DropBoxConnectButton } from './dropbox';
-export {default as ai } from './ai';
+export { AI } from './ai';
 export {default as scrape } from './scrape';
